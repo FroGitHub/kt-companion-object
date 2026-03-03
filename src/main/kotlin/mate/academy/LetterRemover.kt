b750f6d3742c137b9bcc65c5fd84d88349fc618d
FroGitHub <andrey8102310@gmail.com>
2026-03-03 15:34:09 +0100

    solving x1

--- src/main/kotlin/mate/academy/LetterRemover.kt
+++ src/main/kotlin/mate/academy/LetterRemover.kt
@@ -1,5 +1,14 @@
 package mate.academy
 
 class LetterRemover {
-    // implement ...
+    companion object {
+        const val LETTER_R: Char = 'r'
+        const val SYM: Char = '!'
+
+        @JvmStatic fun removeLetter(message: String): CharArray
+        = message
+            .lowercase()
+            .replace(LETTER_R, SYM)
+            .toCharArray()
+    }
 }
